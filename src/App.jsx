6b8ff34f45c414447f063cc0c0977897f65f8f99
Hasmitha Gunarathne <gdhasmithag@gmail.com>
2025-01-04 13:25:15 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to RapidAid!</h1>
+      <p>Your Disaster Management and Emergency Response System is live! 🚨</p>
+    </div>
+  );
+}
+
+export default App;
